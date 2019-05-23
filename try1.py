fe8acd8cc9d21b9301097a1f7ecfe500ce748a70
Kartikrjpl <kartik17157@iiitd.ac.in>
2019-05-23 10:46:29 -0700

    first commit

--- try1.py
+++ try1.py
@@ -1,2 +1,13 @@
 import json
+twiter_cred = dict()
 
+twitter_cred['CONSUMER_KEY'] = '***********************'
+twitter_cred['CONSUMER_SECRET'] = '***********************'
+twitter_cred['ACCESS_KEY'] = '***********************'
+twitter_cred['ACCESS_SECRET'] = '***********************'
+
+with open('twitter_credentials.json', 'w') as secret_info:
+json.dump(twitter_cred, secret_info, indent=4, sort_keys=True)
+
+
+wiufh
